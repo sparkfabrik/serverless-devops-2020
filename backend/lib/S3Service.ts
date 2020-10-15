81a35a0794cb5b25954b6ffdbf9c339c4fa2110f
Francesco Benigno <francesco.benigno@sparkfabrik.com>
2020-10-15 02:33:03 +0200

    6: Add new lambda to process csv file from S3 bucket

--- backend/lib/S3Service.ts
+++ backend/lib/S3Service.ts
@@ -1,3 +1,5 @@
+import * as parse from 'csv-parse';
+
 export class S3Service {
 
   constructor(
@@ -18,6 +20,27 @@ export class S3Service {
         }
         resolve(url);
       });
+    });
+  }
+
+  async GetCsvContent(bucket: string, key: string, columns: string[]): Promise<any[]> {
+    return new Promise<any[]>((resolve, reject) => {
+      const options: parse.Options = {
+        delimiter: ',',
+        columns: columns,
+        from: 2,
+      };
+      const parser = parse(options, (err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+      const params: AWS.S3.GetObjectRequest = { Bucket: bucket, Key: key };
+      this.s3Client
+        .getObject(params)
+        .createReadStream()
+        .pipe(parser);
     });
   }
 
